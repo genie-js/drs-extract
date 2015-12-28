ac668313338f3128612f670f48c0fe3f5f298995
René Kooi <rene@kooi.me>
2015-12-28 14:50:23 +0100

    implementation

---
+++ index.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+'use strict'
+
+var fs = require('fs')
+var path = require('path')
+var each = require('each-async')
+var DRS = require('genie-drs')
+var meow = require('meow')
+var mkdirp = require('mkdirp')
+
+var cli = meow({
+  help: [
+    'Usage',
+    '  $ drs-extract <drs-file> <out-dir>',
+    '',
+    'Options',
+    '  -v, --verbose  List extracted files',
+    'Example',
+    '  $ drs-extract interfac.drs /tmp/interfac/'
+  ]
+}, {
+  alias: {
+    v: 'verbose'
+  }
+})
+
+function run (file, outDir) {
+  var drs = DRS(file)
+  drs.read(function () {
+    each(drs.getFiles(), function (meta, i, next) {
+      var name = meta.id + '.' + meta.type
+      drs.readFile(meta.id, function (e, file) {
+        if (e) return next(e)
+        mkdirp.sync(outDir)
+        fs.writeFileSync(path.join(outDir, name), file.buf)
+        if (cli.flags.verbose) {
+          console.log(meta.type + '/' + meta.id, '→', path.join(outDir, name))
+        }
+        next()
+      })
+    }, function (e) {
+      if (e) throw e
+    })
+  })
+}
+
+run(cli.input[0], cli.input[1])
--- readme.md
+++ readme.md
@@ -2,6 +2,30 @@
 
 Extract Genie (Age of Empires, SWGB) .DRS archives.
 
+## Installation
+
+```bash
+npm install --global drs-extract
+```
+
+## Usage
+
+```bash
+$ drs-extract --help
+
+Usage
+  $ drs-extract <drs-file> <out-dir>
+
+Options
+  -v, --verbose  List extracted files
+Example
+  $ drs-extract interfac.drs /tmp/interfac/
+```
+
+## Programmatic Usage
+
+Use [genie-drs](https://github.com/goto-bus-stop/genie-drs), the API library :)
+
 ## Licence
 
 [MIT](./LICENSE)
