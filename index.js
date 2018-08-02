9a9a6dc9f7565df61ebf85b4978608b5dbfa51d9
Renée Kooi <renee@kooi.me>
2018-08-02 19:20:12 +0200

    Add examples to help text.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 #!/usr/bin/env node
 var meow = require('meow')
 
-var cli = meow(`
-  Usage:
-    $ drs-extract --create /path/to/archive.drs
-    $ drs-extract /path/to/archive.drs [--extract|--get|--add|--list] [options]
-`, {
+var help = 'Usage:\n' +
+  '  $ drs-extract --create /path/to/archive.drs\n' +
+  '  $ drs-extract /path/to/archive.drs [--extract|--get|--add|--list] [options]\n' +
+  '\n' +
+  'Examples:\n' +
+  '  Extract all files from an archive to a folder:\n' +
+  '  $ drs-extract /path/to/archive.drs --extract /path/to/output/folder/\n' +
+  '\n' +
+  '  Print the contents of a single file to stdout:\n' +
+  '  $ drs-extract /path/to/archive.drs --get 51000\n' +
+  '\n' +
+  '  Add a file to an archive:\n' +
+  '  $ drs-extract /path/to/archive.drs --table bina --add 51000 /path/to/palette.pal\n' +
+  '  $ generate-file | drs-extract /path/to/archive.drs --table bina --add 32533\n' +
+  '\n' +
+  '  Print a list of files in the archive:\n' +
+  '  $ drs-extract /path/to/archive.drs --list\n'
+
+var cli = meow(help, {
   flags: {
     create: {
       type: 'string',
